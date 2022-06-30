e4da8b6ccfdc7c9995e99d91103624204fe62ce7
domiguo818 <1193224158@qq.com>
2022-06-30 21:29:36 +0800

    完成day3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <span>姓名:</span>
+      <input type="text" />
+    </div>
+    <div>
+      <span>年龄:</span>
+      <input type="number" />
+    </div>
+    <div>
+      <span>性别:</span>
+      <select >
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </select>
+    </div>
+    <div>
+      <button >添加/修改</button>
+    </div>
+    <div>
+      <table
+        border="1"
+        cellpadding="10"
+        cellspacing="0"
+      >
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="item in arr" :key="item.id">
+          <th>{{item.id}}</th>
+          <th>{{item.name}}</th>
+          <th>{{item.age}}</th>
+          <th>{{item.sex}}</th>
+          <td>
+            <button >删除</button>
+            <button >编辑</button>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      arr:[{id:1,name:'chaochao',age:18,sex:'男'},
+      {id:2,name:'qianqian',age:18,sex:'女'},
+      {id:3,name:'lulu',age:18,sex:'男'},
+      {id:4,name:'nannan',age:18,sex:'男'},
+      {id:5,name:'wenwen',age:18,sex:'男'},
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/大学起点.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ul>
+      <li :class="{active:index==currtIndex}" v-for="(item,index) in navs" :key="item" @click='toggle(index)'>{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+      navs: ['大学起点', '高中起点', '初中起点', '小学起点'],
+      currtIndex:0
+    }
+  },
+  methods: {
+  toggle(val){
+    this.currtIndex = val
+  }
+  },
+}
+</script>
+
+<style>
+  ul {
+    list-style: none;
+    border-radius: 10px;
+    width: 400px;
+    overflow: hidden;
+    padding: 0;
+  }
+  ul li {
+    float: left;
+    width: 100px;
+    height: 40px;
+    background-color: #ccc;
+    color: #fff;
+    text-align: center;
+    line-height: 40px;
+    cursor: pointer;
+  }
+  li.active {
+    background-color: blue;
+  }
+</style>
